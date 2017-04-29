b7c5d839c6bb5ed42a991e45510bcc118d095cfb
Naya Verdier <nayamichaelv@gmail.com>
2017-04-28 18:34:09 -0700

    improve

--- pixels/main.js
+++ pixels/main.js
@@ -1,50 +1,68 @@
 function randomHex() {
-  return 255 * Math.random() | 0;
+    return 255 * Math.random() | 0; // | 0 to convert to int
 }
 
 function randomColor(transparent) {
-  var r = randomHex(),
-      g = randomHex(),
-      b = randomHex(),
-      a = Math.random();
-  return 'rgb' + (transparent ? 'a(' : '(') + r + ', ' + g + ', ' + b + (transparent ? ', ' + a : '') + ')';
+    var r = randomHex(),
+        g = randomHex(),
+        b = randomHex(),
+        a = Math.random();
+
+    // Use rgba if transparency is enabled, otherwise just use rgb
+    if (transparent)
+        return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + a + ')';
+    else
+        return 'rgb(' + r + ', ' + g + ', ' + b + ')';
 }
 
-function generate() {
-  var width = parseInt(document.getElementById('widthInput').value);
-  var transparent = document.getElementById('transparent').checked;
-  var height = parseInt(document.getElementById('heightInput').value);
-  var pixel = parseInt(document.getElementById('pixelInput').value);
-  var ctx = canvas.getContext('2d');
-  var x, y = 0;
+function generate(draw = true) {
+	// Get values from forms
+    var width = parseInt(document.getElementById('widthInput').value);
+    var transparent = document.getElementById('transparent').checked;
+    var height = parseInt(document.getElementById('heightInput').value);
+    var pixel = parseInt(document.getElementById('pixelInput').value);
 
-  width = width - (width % pixel);
-  height = height - (height % pixel);
+    // Create pre-render canvas for efficiency
+    var prerender = document.createElement('canvas');
+    var preCtx = prerender.getContext('2d');
 
-  ctx.canvas.width = width;
-  ctx.canvas.height = height;
+    // Round canvas down to the nearest "pixel"
+    preCtx.canvas.width = width -= (width % pixel);
+    preCtx.canvas.height = height -= (height % pixel);
 
-  for(; y < height; y += pixel) {
-    for(x = 0; x < width; x += pixel) {
-        ctx.fillStyle = randomColor(transparent);
-        ctx.fillRect(x, y, pixel, pixel);
+    // Set each pixel
+    for (var y = 0; y < height; y += pixel) {
+        for (var x = 0; x < width; x += pixel) {
+            preCtx.fillStyle = randomColor(transparent);
+            preCtx.fillRect(x, y, pixel, pixel);
+        }
     }
-  }
-  
-  document.getElementById('saveButton').disabled = "";
+
+    // Add to page if necessary
+    if (draw) {
+        var ctx = canvas.getContext('2d');
+        ctx.canvas.width = width;
+        ctx.canvas.height = height;
+        ctx.drawImage(prerender, 0, 0);
+
+        // Enable the save button since its been generated
+        document.getElementById('saveButton').disabled = "";
+    }
 }
 
 function feelingLucky() {
-	generate();
-	save();
-	window.location = canvas.toDataURL("image/png");
+    generate(false); // Don't draw to page
+    save();          // But save it
 }
 
 function save() {
-  var url = canvas.toDataURL("image/png");
-  var link = document.createElement("a");
-  link.download = "random-pixels.png";
-  link.href = url;
-  document.body.appendChild(link);
-  link.click();
+	// Convert to image
+    var url = canvas.toDataURL("image/png");
+
+    // Create download link and click it
+    var link = document.createElement("a");
+    link.download = "random-pixels.png";
+    link.href = url;
+    document.body.appendChild(link);
+    link.click();
 }
